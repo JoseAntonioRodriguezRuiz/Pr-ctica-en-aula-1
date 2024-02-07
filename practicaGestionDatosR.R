11594b68e7348f873b10d17752b97bc92f6d1f7e
JoseAntonioRodriguezRuiz <158162619+JoseAntonioRodriguezRuiz@users.noreply.github.com>
2024-02-07 12:48:41 +0100

    Add files via upload

---
+++ practicaGestionDatosR.R
@@ -0,0 +1,49 @@
+#Ejercicio 1
+edades_descubrimientos <- c(1859,1830,1834,1860,1845)
+mean.difftime(edades_descubrimientos)
+
+#Ejercicio 2
+cantidad_artefactos <- c(5,6,22,17,8)
+valor_suma <- 5+6+22+17+8
+print(valor_suma)
+
+#Ejercicio 3
+profundidad_hallazgos <- c("10","30")
+max(profundidad_hallazgos, na.rm=T)
+
+#Ejercicio 4
+materiales_encontrados <- c("cerámica","vidrio","metal")
+materiales_encontrados <- data.frame(
+  tipo = c(21, 24, 22))
+
+#Ejercicio 5
+años_excavaciones <- c(1986,1980,1978,2000,1991)
+años_excavaciones <- data.frame(
+  años = c(11,20,17,22,30))
+
+#Ejercicio 6
+sitios_arqueológicos <- matrix(data=1:20, nrow=4, ncol=5, byrow=FALSE)
+
+#Ejercicio 7
+
+
+#Ejercicio 8
+
+#Ejercicio 9
+
+#Ejercicio 10
+
+#Ejercicio 11
+
+
+#Ejercicio 12
+
+#Ejercicio 13
+
+#Ejercicio 14
+
+
+
+
+
+
